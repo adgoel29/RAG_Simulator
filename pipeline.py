503ea0db48454b2cda21a2d05986736240940fc6
adgoe29 <adgoel29@gmail.com>
2026-06-02 00:15:28 +0530

    added modified pipeline code

--- pipeline.py
+++ pipeline.py
@@ -5,10 +5,12 @@ from search import SearchEngine
 path="sample.txt"
 model = SentenceTransformer("all-MiniLM-L6-v2")
 test=RagClass("all-MiniLM-L6-v2")
+#semantic or fixed,parameters of fixed are changeable
 chunk=test.chunking(path,"semantic")
 print(chunk)
 test.ingest(chunk)
 query = input("Enter query: ")
-ans = test.search(query)
+#mmr or similarity ,default is similarity,can  use top_k as well.
+ans = test.searching(query,"mmr")
 print(ans)
 
