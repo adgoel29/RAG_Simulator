311dd78d2975879ecbda8d837114f2ff28475ce8
adgoe29 <adgoel29@gmail.com>
2026-06-01 23:54:31 +0530

    added chunking function to rag class

--- faissclass.py
+++ faissclass.py
@@ -5,12 +5,14 @@ from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from search import SearchEngine
 from reranker import Reranker
+from chunking import chunk
 class RagClass:
     def __init__(self,model_name):
         self.vectorstore = None
         self.embeddings = HuggingFaceEmbeddings(
             model_name=model_name#"sentence-transformers/all-MiniLM-L6-v2"
         )
+        self.tochunk=chunk(self.embeddings)
     def ingest(self, chunks):
         self.vectorstore = FAISS.from_texts(
             texts=chunks,
@@ -29,6 +31,17 @@ class RagClass:
             method=method
         )
         return docs
+    def chunking(self,path,method,**kwargs):
+        with open(path,'r',encoding="utf-8") as f:
+            con=f.read()
+        content=con
+        if method=="semantic":
+            chunks=self.tochunk.semantic_chunking(content)
+        elif method=="fixed":
+            chunks=self.tochunk.fixed(content,**kwargs)
+        else:
+            pass
+        return chunks
     def rerank(self,query,docs,topk=3,model_name=None):
         if not query or not docs or not model_name:
             raise ValueError("query,docs and model_name are required")
